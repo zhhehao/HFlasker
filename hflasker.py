2c75565f069c090a0981a62fac0339b7cda86727
zhhehao <zh.hehao@gmail.com>
2016-10-08 21:40:28 +0800

    completed view parts

--- hflasker.py
+++ hflasker.py
@@ -33,6 +33,58 @@ def get_db():
 
 # Closes the database again at the end of the request.
 @app.teardown_appcontext
-def close_db():
-	if hasattr(g, 'sqlite_db')
+def close_db(error):
+	if hasattr(g, 'sqlite_db'):
 		g.sqlite_db.close()
+
+def init_db():
+	db = get_db()
+	with app.open_resource('schema.sql', mode='r') as f:
+		db.cursor().executescript(f.read())
+	db.commit()
+
+# Initializes the database.
+@app.cli.command('initdb')
+def initdb_command():
+	init_db()
+	print('Initialized the database.')
+
+# Show entries
+@app.route('/')
+def show_entries():
+	db = get_db()
+	cur = db.execute('select title, text from entries order by id desc')
+	entries = cur.fetchall()
+	return render_template('show_entries.html', entries=entries)
+
+# Add entries
+@app.route('/add', methods=['POST'])
+def add_entry():
+		if not session.get('logged_in'):
+			abort(401)
+		db = get_db()
+		db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
+		db.commit()
+		flash('New entry was successfully posted.')
+		return redirect(url_for('show_entries'))
+
+# Login and logout
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
